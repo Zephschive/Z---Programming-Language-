9f987bca0911c7ba746a66f28288010d20503ba7
Zephschive <zephaniahollennu@gmail.com>
2024-06-19 18:18:40 +0000

    committed

--- basic.py
+++ basic.py
@@ -4,22 +4,31 @@ DIGITS = '0123456789'
 LETTERS = string.ascii_letters
 LETTERS_DIGITS = LETTERS + DIGITS
 
-TT_INT = "TT_INT"
-TT_FLOAT = "TT_FLOAT"
-TT_PLUS = 'TT_PLUS'
-TT_MINUS = "TT_MINUS"
-TT_MUL  = 'TT_MUL'
-TT_DIV = 'TT_DIV'
+#TOKENS
+
+TT_INT = "INT"
+TT_FLOAT = "FLOAT"
+TT_PLUS = 'PLUS'
+TT_MINUS = "MINUS"
+TT_MUL  = 'MUL'
+TT_DIV = 'DIV'
 TT_POW = 'POW'
-TT_LPAREN = 'TT_LPAREN'
-TT_RPAREN = 'TT_RPAREN'
-TT_EOF = 'TT_EOF'
+TT_LPAREN = 'LPAREN'
+TT_RPAREN = 'RPAREN'
+TT_EOF = 'EOF'
 TT_KEYWORD = 'IDENTIFIER'
 TT_EQ = '='
+TT_EE = 'EE'
+TT_NE = 'NE'
+TT_LT = 'LT'
+TT_GT = 'GT'
+TT_LTE = 'LTE'
+TT_GTE = 'GTE'
+
 TT_IDENTIFIER = 'IDENTIFIER'
 
 KEYWORDS = [
-    'zvar'
+    'zvar','zand','zor','znot'
 ]
 
 class Error:
@@ -63,6 +72,10 @@ class IllegalCharError(Error):
 class InvalidSyntaxError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+
+class ExpectedCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Expected Character', details)
 
 class RTError(Error):
     def __init__(self, pos_start, pos_end, details, context):
@@ -192,7 +205,7 @@ class Lexer:
                 tokens.append(Token(TT_DIV, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '=':
-                tokens.append(Token(TT_EQ, pos_start=self.pos))
+                tokens.append(self.make_equals())
                 self.advance()
             elif self.current_char == '^':
                 tokens.append(Token(TT_POW, pos_start=self.pos))
@@ -202,6 +215,16 @@ class Lexer:
                 self.advance()
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '!':
+                tok, error = self.make_not_equals()
+                if error: return [], error
+                tokens.append(tok)
+            elif self.current_char == '<':
+                tokens.append(self.make_less_than())
+                self.advance()
+            elif self.current_char == '>':
+                tokens.append(self.make_greater_than())
                 self.advance()
             else:
                 pos_start = self.pos.copy()
@@ -241,6 +264,51 @@ class Lexer:
 
         tok_type = TT_KEYWORD if id_str in KEYWORDS else TT_IDENTIFIER
         return Token(tok_type, id_str, pos_start, self.pos)
+
+    def make_not_equals(self):
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            return Token(TT_NE, pos_start=pos_start, pos_end=self.pos), None
+
+        self.advance()
+        return  None, ExpectedCharError(pos_start, self.pos, "'=' (after '!')")
+
+    def make_equals(self):
+        tok_type = TT_EQ
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = TT_EE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+    def make_less_than(self):
+        tok_type = TT_LT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = TT_LTE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+    def make_greater_than(self):
+        tok_type = TT_GT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = TT_GTE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
 
 
 class RTResult:
@@ -381,6 +449,22 @@ class Interpreter:
             result, error = left.dived_by(right)
         elif node.op_tok.type == TT_POW:
             result, error = left.powed_by(right)
+        elif node.op_tok.type == TT_EE:
+            result, error = left.get_comparison_eq(right)
+        elif node.op_tok.type == TT_NE:
+            result, error = left.get_comparison_ne(right)
+        elif node.op_tok.type == TT_LT:
+            result, error = left.get_comparison_lt(right)
+        elif node.op_tok.type == TT_GT:
+            result, error = left.get_comparison_gt(right)
+        elif node.op_tok.type == TT_LTE:
+            result, error = left.get_comparison_lte(right)
+        elif node.op_tok.type == TT_GTE:
+            result, error = left.get_comparison_gte(right)
+        elif node.op_tok.matches(TT_KEYWORD,'zand'):
+            result, error = left.anded(right)
+        elif node.op_tok.type == TT_POW:
+            result, error = left.ored_by(right)
 
         if error:
             return res.failure(error)
@@ -395,6 +479,9 @@ class Interpreter:
         error = None
         if node.op_tok.type == TT_MINUS:
             number, error = number.multed_by(Number(-1))
+        elif node.op_tok.matches(TT_KEYWORD, 'znot'):
+            number, error = number.notted()
+
         if error:
             return res.failure(error)
         else:
@@ -436,6 +523,43 @@ class Number:
     def powed_by(self, other):
         if isinstance(other,Number):
             return Number(self.value ** other.value).set_context(self.context), None
+
+
+
+    def get_comparison_eq(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value == other.value)).set_context(self.context), None
+
+    def get_comparison_ne(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value != other.value)).set_context(self.context), None
+
+    def get_comparison_lt(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value < other.value)).set_context(self.context), None
+
+    def get_comparison_gt(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value > other.value)).set_context(self.context), None
+
+    def get_comparison_lte(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value <= other.value)).set_context(self.context), None
+
+    def get_comparison_gte(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value >= other.value)).set_context(self.context), None
+
+    def anded_by(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value and other.value)).set_context(self.context), None
+
+    def ored_by(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value or other.value)).set_context(self.context), None
+
+    def notted(self):
+        return Number(True if self.value == 0 else False).set_context(self.context), None
 
 
     def copy(self):
@@ -522,6 +646,31 @@ class Parser:
     def term(self):
         return self.bin_op(self.factor, (TT_MUL, TT_DIV))
 
+    def comp_expr(self):
+        res = ParseResult()
+
+        if self.current_tok.matches(TT_KEYWORD,'znot'):
+           op_tok = self.current_tok
+           res.register_advancement()
+           self.advance()
+
+           node = res.register(self.comp_expr())
+           if res.error : return res
+           return res.success(UnaryOpNode(op_tok, node))
+
+        node = res.register(self.bin_op(self.arith_expr,(TT_EE, TT_NE, TT_LT, TT_GT, TT_LTE, TT_GTE)))
+
+        if res.error:
+            return res.failure(InvalidSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end,
+              "Expected int, float, identifier, 'znot','+','-' or '('"))
+
+        return res.success(node)
+
+
+    def arith_expr(self):
+        return self.bin_op(self.term,(TT_PLUS,TT_MINUS))
+
+
     def expr(self):
         res = ParseResult()
 
@@ -552,7 +701,7 @@ class Parser:
 
             return res.success(VarAssignNode(var_name, expr))
 
-        node = res.register( self.bin_op(self.term, (TT_PLUS, TT_MINUS)))
+        node = res.register( self.bin_op(self.comp_expr, ((TT_KEYWORD,"zand"),(TT_KEYWORD,"zor"))))
 
         if res.error :
             return res.failure(InvalidSyntaxError(
@@ -570,7 +719,7 @@ class Parser:
         left = res.register(func_a())
         if res.error: return res
 
-        while self.current_tok.type in ops:
+        while self.current_tok.type in ops or (self.current_tok.type, self.current_tok.value) in ops:
             op_tok = self.current_tok
             res.register_advancement()
             self.advance()
